aca0df5e0bf71e34aeacf1e3a7ccb711b26b045b
Aare Undo <aare@carto.com>
2017-03-22 15:58:24 +0200

    iOS: added file writing logic

---
+++ ios/MCFileWriterUtil.h
@@ -0,0 +1,13 @@
+//
+//  MCFileWriterUtil.h
+//  CustomComponent
+//
+//  Created by Jesus Garcia on 3/30/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import <React/RCTRootView.h>
+
+@interface MCFileWriterUtil : NSObject <RCTBridgeModule>
+
+@end
